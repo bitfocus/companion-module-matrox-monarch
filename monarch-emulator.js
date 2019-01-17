cad1e9aede93ea715592a5823a102c7fe58a8c01
Todd Eichel <todd@toddeichel.com>
2019-01-17 01:33:18 -0800

    Quick and dirty Monarch emulator to test against

---
+++ monarch-emulator.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 80
+
+const apiPath = '/Monarch/syncconnect/sdk.aspx'
+const sampleResponse = `
+  <Light>False</Light>
+  <ErrorMessage></ErrorMessage>
+  <encoder1>False,RTMP</encoder1>
+  <encoder2>False,RTMP</encoder2>
+  <width>1920</width>
+  <height>1080</height>
+  <frameRate>60</frameRate>
+  <frameRateM>59.94</frameRateM>
+  <scanMode>False</scanMode>
+  <videoInput>SDI</videoInput>
+  <audioRate>48 kHz</audioRate>
+  <audioInput>SDI</audioInput>
+  <audioPresent>True</audioPresent>
+  <fanFault>False</fanFault>
+  <tempFault>False</tempFault>
+  <CCDigitalPresent>False</CCDigitalPresent>
+  <CCAnalogPresent>False</CCAnalogPresent>
+`;
+
+app.get(apiPath, (request, response) => {
+  console.log(`Received request: ${request.originalUrl}`)
+  response.send(sampleResponse)
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`Monarch emulator is listening on ${port}`)
+})
